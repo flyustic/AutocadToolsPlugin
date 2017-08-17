7d358a58a503fe6aa2b864c9d8882184fafb3df1
Flyustic <flyustikovv@gmail.com>
2017-08-18 02:46:36 +0300

    added command FIELDSBYTEXTS

--- AutocadToolsPlugin/Commands.cs
+++ AutocadToolsPlugin/Commands.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Globalization;
+using System.Collections.Generic;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.Runtime;
-
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
 
 namespace AutocadToolsPlugin
 {
@@ -23,7 +25,32 @@ namespace AutocadToolsPlugin
         [CommandMethod("NUMERABLETEXT")]
         public void AutoNumerateCommand()
         {
-            Tools.CreateAutoNumerateText();
+            var ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            var prefix = ed.GetString(new PromptStringOptions("set prefix")).StringResult;
+            var offsetOpt = new PromptIntegerOptions("Offset")
+            {
+                AllowNone = false,
+                AllowZero = true,
+                AllowNegative = false,
+                DefaultValue = 1
+            };
+            var offset = ed.GetInteger(offsetOpt).Value;
+            var pPtOpts = new PromptPointOptions("\ninsert point:");
+
+            while (true)
+            {
+                var pPtRes = ed.GetPoint(pPtOpts);
+                if (pPtRes.Status == PromptStatus.Cancel) break;
+                var position = pPtRes.Value;
+                var text = new DBText
+                {
+                    Position = position,
+                    TextString = string.Format("{0}{1}", prefix, offset)
+                };
+                Tools.DrawDBtext(text);
+                offset = offset + 1;
+                pPtOpts.Message = "\ninsert next point:";
+            }
         }
         
         [CommandMethod("НУМЕРАШКА")]
@@ -35,8 +62,33 @@ namespace AutocadToolsPlugin
         [CommandMethod("SUMMARIZETEXT", CommandFlags.UsePickSet)]
         public void SumDigitsFromTextObjects()
         {
+            
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
             var editor = Application.DocumentManager.MdiActiveDocument.Editor;
-            var digits = Tools.GetListDigits(Tools.GetTextBySelection());
+            var totalText = " ";
+            var selecttionPrompt = editor.GetSelection();
+            if (selecttionPrompt.Status != PromptStatus.OK) return;
+            using (var transaction = db.TransactionManager.StartTransaction())
+            {
+                foreach (SelectedObject selectedObject in selecttionPrompt.Value)
+                {
+                    if (selectedObject == null) continue;
+                    var dbText = transaction.GetObject(selectedObject.ObjectId, OpenMode.ForRead) as DBText;
+
+                    if (dbText != null)
+                    {
+                        totalText = totalText + " " + dbText.TextString;
+                    }
+                    else
+                    {
+                        var mText = transaction.GetObject(selectedObject.ObjectId, OpenMode.ForRead) as MText;
+                        if (mText == null) continue;
+                        totalText = totalText + " " + mText.Text;
+                    }
+                }
+            }
+            var digits = Tools.GetListDigits(totalText);
             var result = 0.0;
             var cInfo = new CultureInfo("ru-RU");
             foreach (var digit in digits)
@@ -52,6 +104,48 @@ namespace AutocadToolsPlugin
         public void SumDigitsFromTextObjectsAlias()
         {
             SumDigitsFromTextObjects();
-        }       
+        }
+
+        [CommandMethod("FIELDSBYTEXTS", CommandFlags.UsePickSet)]
+        public void GetFormatForManyFields()
+        {
+            var doc = Application.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
+            var editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            var text = "";
+            const string separator = ";";
+            const string template = "%<\\AcObjProp Object(%<\\_ObjId {0}>%).TextString>%";
+            
+            var selecttionPrompt = editor.GetSelection();
+            if (selecttionPrompt.Status != PromptStatus.OK) return;
+            
+            using (var transaction = db.TransactionManager.StartTransaction())
+            {
+
+                foreach (SelectedObject selectedObject in selecttionPrompt.Value)
+                {
+                    if (selectedObject == null) continue;
+                    var dbText = transaction.GetObject(selectedObject.ObjectId, OpenMode.ForRead) as DBText;
+                    if (dbText != null)
+                    {
+                        text += string.Format(template, dbText.ObjectId.OldIdPtr) + separator;
+                    }
+                    else
+                    {
+                        var mText = transaction.GetObject(selectedObject.ObjectId, OpenMode.ForRead) as MText;
+                        if (mText == null) continue;
+                        
+                        text += string.Format(template, mText.ObjectId.OldIdPtr) + separator;
+                    }
+                }
+            }
+            editor.WriteMessage(string.Format("============================\n{0}\n============================", text));
+        }
+        
+        [CommandMethod("ФИЛДЫ", CommandFlags.UsePickSet)]
+        public void GetFormatForManyFieldsAlias()
+        {
+            GetFormatForManyFields();
+        }
     }
 }
--- AutocadToolsPlugin/Tools.cs
+++ AutocadToolsPlugin/Tools.cs
@@ -5,101 +5,30 @@ using System.Globalization;
 using System.Linq;
 using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.EditorInput;
-using Autodesk.AutoCAD.Geometry;
 
 
 namespace AutocadToolsPlugin
 {
     public class Tools
     {
-        public static void CreateAutoNumerateText()
-        {
-            var acDoc = Application.DocumentManager.MdiActiveDocument;
-            var acDb = acDoc.Database;
-            var ed = Application.DocumentManager.MdiActiveDocument.Editor;
-
-            var fixOpt = new PromptStringOptions("set prefix");
-            var st = ed.GetString(fixOpt);
-            var fixTextValue = st.StringResult;
-
-            var optNotFix = new PromptIntegerOptions("Offset")
-            {
-                AllowNone = false,
-                AllowZero = true,
-                AllowNegative = false,
-                DefaultValue = 0
-            };
-            var st1 = ed.GetInteger(optNotFix);
-            var beginInt = st1.Value;
-
-            var pPtOpts = new PromptPointOptions("Insert point\n");
-
-            while (true)
-            {
-                using (var acTrans = acDb.TransactionManager.StartTransaction())
-                {
-                    var pPtRes = ed.GetPoint(pPtOpts);
-                    var pos = pPtRes.Value;
-                    if (pPtRes.Status == PromptStatus.Cancel) break;
-                    beginInt = beginInt + 1;
-                    CreateAcText(fixTextValue, beginInt, pos, acTrans);
-                    acTrans.Commit();
-                }
-            }
-        }
-
-        public static void CreateAcText(string fix, int number, Point3d pos, Transaction acTrans)
-        {
-            var acDoc = Application.DocumentManager.MdiActiveDocument;
-            var acDb = acDoc.Database;
-            var blockTable = acTrans.GetObject(acDb.BlockTableId, OpenMode.ForRead) as BlockTable;
-            var blockTableRec = acTrans.GetObject(blockTable[BlockTableRecord.ModelSpace],
-                OpenMode.ForWrite) as BlockTableRecord;
-            var text = new DBText
-            {
-                Position = pos,
-                TextString = fix + Convert.ToString(number)
-            };
-
-            blockTableRec.AppendEntity(text);
-            acTrans.AddNewlyCreatedDBObject(text, true);
-        }
-
         /// <summary>
-        /// Gets MTetx and DBText objects from autocad's selection, and convert to string
+        /// Draw DBText in autocad
         /// </summary>
-        /// <returns>string</returns>
-        public static string GetTextBySelection()
+        /// <param name="txt">DBText object</param>
+        public static void DrawDBtext(DBText text)
         {
             var doc = Application.DocumentManager.MdiActiveDocument;
             var db = doc.Database;
-            var editor = Application.DocumentManager.MdiActiveDocument.Editor;
-            var result = " ";
-            using (var transaction = db.TransactionManager.StartTransaction())
+            using (var tr = db.TransactionManager.StartTransaction())
             {
-                var selecttionPrompt = editor.GetSelection();
+                var blockTable = tr.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                var tableRec =
+                    tr.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
 
-                if (selecttionPrompt.Status != PromptStatus.OK) return result;
-
-                foreach (SelectedObject selectedObject in selecttionPrompt.Value)
-                {
-                    if (selectedObject == null) continue;
-                    var dbText = transaction.GetObject(selectedObject.ObjectId, OpenMode.ForRead) as DBText;
-
-                    if (dbText != null)
-                    {
-                        result = result + " " + dbText.TextString;
-                    }
-                    else
-                    {
-                        var mText = transaction.GetObject(selectedObject.ObjectId, OpenMode.ForRead) as MText;
-                        if (mText == null) continue;
-                        result = result + " " + mText.Text;
-                    }
-                }
+                tableRec.AppendEntity(text);
+                tr.AddNewlyCreatedDBObject(text, true);
+                tr.Commit();
             }
-            return result;
         }
 
         /// <summary>
